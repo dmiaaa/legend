0ba530d4a0ed8ed1204997a429ff090f63dcd3d5
Nur Damia <damiaqhairil03@gmail.com>
2023-02-07 16:24:32 +0800

    semakan pertama

---
+++ hello.py
@@ -0,0 +1,22 @@
+#Nur Damia 
+#019-8740197
+#damiaqhairil@gmail.com
+
+#name = input("What is your name?\n")
+#greeting=("Hi " + name)
+#price_goods = float(input ("How many is the amount to pay?\n"))
+#tax = float(input ("How many percent is the sales tax?\n"))
+#sales_tax = tax/100 * goods
+#sales_amount = salesTax + goods
+#print(greeting + name + ", the total amount to pay is RM" + str(salesAmount))
+
+
+#Nur Damia
+#20DDT21F1001
+#Celcius to Farenheit conversion
+
+celcius=float(input("Enter celcius: \n"))
+farenheit=float((celcius*9/5)+32)
+print("Conversion from celcius to farenheit is " +str(farenheit))
+
+
