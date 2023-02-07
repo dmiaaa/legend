8bc80dd018f8f51da06008a0d931aa3518b1e71e
Nur Damia <damiaqhairil03@gmail.com>
2023-02-07 16:32:01 +0800

    Perubahan kedua

--- hello.py
+++ hello.py
@@ -6,9 +6,9 @@
 #greeting=("Hi " + name)
 #price_goods = float(input ("How many is the amount to pay?\n"))
 #tax = float(input ("How many percent is the sales tax?\n"))
-#sales_tax = tax/100 * goods
-#sales_amount = salesTax + goods
-#print(greeting + name + ", the total amount to pay is RM" + str(salesAmount))
+#sales_tax = tax/100 *price_goods
+#sales_amount = sales_tax + price_goods
+#print(greeting + name + ", the total amount to pay is RM" + str(sales_amount))
 
 
 #Nur Damia
@@ -19,4 +19,5 @@ celcius=float(input("Enter celcius: \n"))
 farenheit=float((celcius*9/5)+32)
 print("Conversion from celcius to farenheit is " +str(farenheit))
 
+#haiiiii
 
